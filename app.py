ffc8f536a8a0c6e9652ddb3987fa47505cb51426
huylinh1997 <huyhuong19972015@gmail.com>
2024-01-21 23:48:49 +0700

    change it

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Harness from Nguyen Dinh Huy- Leo Nguyen 10 1997 Finnalyyyyyyy'
+    return 'Hello Harness from Nguyen Dinh Huy- Leo Nguyen NAB'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
